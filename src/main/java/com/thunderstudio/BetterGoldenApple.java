77d30f609c99e99d48c62476f22644994c9b8ea4
hui_cat <hui_zai2007@outlook.com>
2023-01-11 08:56:07 +0800

    git init

---
+++ src/main/java/com/thunderstudio/BetterGoldenApple.java
@@ -0,0 +1,17 @@
+package com.thunderstudio;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterGoldenApple extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
